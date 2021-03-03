eec59e88fe4d57104f5ce886bd6fd4e0ebf54e34
Julian443 <Lerois88>
2021-03-03 11:32:19 +0100

    jquery done

--- JQuery/js/script.js
+++ JQuery/js/script.js
@@ -1,3 +1,5 @@
+
+
 $("h1").css("color","red");
 $("h2.blue").css("color","green");
 $("h3#second").css("color","red");
@@ -5,3 +7,13 @@ $("p:first").text("New paragraph text");
 $("p:last").wrap("<em></em>");
 $("ul").append("<li id='four'>Hello new List Element</li>");
 jQuery("li#four").prop("id","five");
+$("li").each(function) {
+    var $id = this.dispatchEvent;
+    $(this).append("<em>" + $id + "</em>")
+}
+$("li").css("font-size","30px");
+$(document).ready(function(){
+    $("li").click(function(){
+      $("li").remove();
+    });
+  });
