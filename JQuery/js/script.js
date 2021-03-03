1c756813361520b8dae551f36406a66ea3ce1103
Julian443 <Lerois88>
2021-03-03 09:57:51 +0100

    jquery

--- JQuery/js/script.js
+++ JQuery/js/script.js
@@ -1,2 +1,6 @@
 $("h1").css("color","red");
 $("h2.blue").css("color","green");
+$("h3#second").css("color","red");
+$("p:first").text("New paragraph text");
+$("p:last").wrap("<em></em>");
+$("ul").append("<li id='four'>Hello new List Element</li>");
