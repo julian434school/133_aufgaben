443dfb52238d1f1624c8ce29770a9cb5ff4da5be
julian443 <63303982+julian443@users.noreply.github.com>
2021-03-17 10:11:45 +0100

    ajaxjson

--- AJAX JSON/ajax.js
+++ AJAX JSON/ajax.js
@@ -1,5 +1,6 @@
 var personObj = new Object();
 
+/*
 function suchen() {
     fetch("https://gibm.becknet.ch/personaldaten/personal.php?name=julian")
   .then(response => response.json())
@@ -9,6 +10,29 @@ function suchen() {
   $("p#queryresults").append(personObj.success);
 
 }
+*/
+/*
+function search() {
+  $.get("https://gibm.becknet.ch/personaldaten/personal.php?name=julian", function (data) {
+    $("p#queryresults").append(data);
+    alert("Load was performed.");
+  });
+}
+*/
 
+function search() {
+    $.getJSON('https://gibm.becknet.ch/personaldaten/personal.php?name=julian', function (data) {
+      $("#queryresults").text(data.data[0].vorname);
+    });
+}
 
+/*
+(document).ready(function () {
+  $("searchButton").click(function () {
+    $.getJSON('http://query.yahooapis.com/v1/public/yql?q=select%20%2a%20from%20yahoo.finance.quotes%20WHERE%20symbol%3D%27WRC%27&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys&callback', function (data) {
+      $("p#queryresults").append(data);
+    });
+  });
+});
+*/
 
